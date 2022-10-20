8c85566c0f9cac7bce493092b4fdb298399aa523
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-10-20 06:16:51 -0500

    modify the code to use local storage

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,8 @@ var longitude;
 var j = 0;
 const userCityInput = document.querySelector("#city-input");
 
-
-const currentWeather = 
+const weatherForecast = [
+  //current day
   { 
     city: "",
     date: "",
@@ -14,9 +14,7 @@ const currentWeather =
     temperature: "",
     wind: "",
     humidity: "",
-  };
-
-const weatherForecast = [
+  },
   //day 1
   { 
     city: "",
@@ -64,44 +62,36 @@ const weatherForecast = [
   },
 ];
 
-
-//API call by city name
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-
-//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-
-//create query URL for the city's latitude and longitude
-
-
-
+//get user input for city name
 function getCityInput (event){
   event.preventDefault();
-  city = userCityInput.value.trim();
 
-  console.log(city);
-
+  city = userCityInput.value.trim();  
+  
+  clearData();
   firstQuery();
 
   //clear data
   userCityInput.value = "";
 }
 
+//clear data before
+function clearData(){
 
-//event listener for search button
-document.getElementById("search-btn").addEventListener("click",getCityInput);
+
+}
 
 
 //first query function
 function firstQuery(){
 
-  //build first query URL
+  //build first query URL with imperial unit
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
-  fetch(queryURL)
-    .then(function (response){
+  fetch(queryURL,{
+    cache: 'reload'
+  })
+    .then(function (response,){
       return response.json();
     })
     .then(function(data){
@@ -109,12 +99,12 @@ function firstQuery(){
       longitude = data.coord.lon;
  
       //get data from server    
-      currentWeather.city = data.name;
-      currentWeather.date = moment.unix(data.dt).format('L');
-      currentWeather.icon = data.weather[0].icon;
-      currentWeather.temperature = data.main.temp;
-      currentWeather.wind = data.wind.speed;
-      currentWeather.humidity = data.main.humidity;
+      weatherForecast[0].city = data.name;
+      weatherForecast[0].date = moment.unix(data.dt).format('L');
+      weatherForecast[0].icon = data.weather[0].icon;
+      weatherForecast[0].temperature = data.main.temp;
+      weatherForecast[0].wind = data.wind.speed;
+      weatherForecast[0].humidity = data.main.humidity;
     
       //build current weather function
       buildCurrentWeather();
@@ -130,146 +120,103 @@ function buildCurrentWeather(){
 
   //build header
   const currentHeader = document.createElement("h2");
-  currentHeader.innerText = currentWeather.city + " " + currentWeather.date + " " + currentWeather.icon;
+  currentHeader.innerText = weatherForecast[0].city + " " + weatherForecast[0].date + " " + weatherForecast[0].icon;
   document.getElementById("currentHeader").appendChild(currentHeader);
 
   //build temperature
   const currentTemp = document.createElement("li");
-  currentTemp.innerText = currentWeather.temperature + " \xBAF"
+  currentTemp.innerText = weatherForecast[0].temperature + " \xBAF"
   document.getElementById("currentBody").appendChild(currentTemp);
 
   //build wind
   const currentWind = document.createElement("li");
-  currentWind.innerText = currentWeather.wind + " MPH"
+  currentWind.innerText = weatherForecast[0].wind + " MPH"
   document.getElementById("currentBody").appendChild(currentWind);
 
   //build humidity
   const currentHumidity = document.createElement("li");
-  currentHumidity.innerText = currentWeather.wind + " %"
+  currentHumidity.innerText = weatherForecast[0].wind + " %"
   document.getElementById("currentBody").appendChild(currentHumidity);
 }
-
-
 
 //second query function
 function secondQueURL(){
 
-  //build second query URL
+  //build second query URL with imperial unit
   var secondQueURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+ latitude + "&lon=" + longitude + "&list=5&appid=" + APIKey + "&units=imperial";
 
   fetch(secondQueURL)
   .then(function (response){
+    console.log(response);
     return response.json();
+   
   })
   .then(function(data){
     console.log(data);
     
-    for (var i = 0; i < weatherForecast.length;i++){
-      j = i * 8;
+    for (var i = 1; i < weatherForecast.length;i++){
+      j = i * 7 + 1;
       weatherForecast[i].city = data.city.name;
       weatherForecast[i].date = moment.unix(data.list[j].dt).format('L');
       weatherForecast[i].icon = data.list[j].weather[0].icon;
       weatherForecast[i].temperature = data.list[j].main.temp;
       weatherForecast[i].wind = data.list[j].wind.speed;
       weatherForecast[i].humidity = data.list[j].main.humidity;
-      /*
-      console.log(weatherForecast[i].city);
-      console.log(weatherForecast[i].date);
-      console.log(weatherForecast[i].icon);
-      console.log(weatherForecast[i].temperature);
-      console.log(weatherForecast[i].wind);
-      console.log(weatherForecast[i].humidity);
-      */
+      
     }
     //build five days weather forecast
     buildWeatherForecast();
   })
     
-    
 }
-
-
-
-
 
 function buildWeatherForecast(){
 
-  for (var i = 0; i<weatherForecast.length;i++){
-
-    var idString ="";
-    idString = 0;
+  for (var i = 1; i<weatherForecast.length;i++){
+    
+    var idString = 1;
 
     //build date
     var futureHeader = document.createElement("h3");
     futureHeader.innerText = weatherForecast[i].date;
-    document.getElementById(idString).appendChild(futureHeader);
+    document.getElementById(idString).append(futureHeader);
 
     //build temperature
     var futureTemp = document.createElement("li");
     futureTemp.innerText = weatherForecast[i].temperature + " \xBAF"
-    document.getElementById(idString).appendChild(futureTemp);
+    document.getElementById(idString).append(futureTemp);
 
     //build wind
     var futureWind = document.createElement("li");
     futureWind.innerText = weatherForecast[i].wind + " MPH"
-    document.getElementById(idString).appendChild(futureWind);
+    document.getElementById(idString).append(futureWind);
 
     //build humidity
     var futureHumidity = document.createElement("li");
     futureHumidity.innerText = weatherForecast[i].wind + " %"
-    document.getElementById("currentBody").appendChild(futureHumidity);
+    document.getElementById(idString).append(futureHumidity);
     
   }
 
-
-/*
-
-
-  //build temperature
-  const currentTemp = document.createElement("li");
-  currentTemp.innerText = currentWeather.temperature + " \xBAF"
-  document.getElementById("currentBody").appendChild(currentTemp);
-
-  //build wind
-  const currentWind = document.createElement("li");
-  currentWind.innerText = currentWeather.wind + " MPH"
-  document.getElementById("currentBody").appendChild(currentWind);
-
-  //build humidity
-  const currentHumidity = document.createElement("li");
-  currentHumidity.innerText = currentWeather.wind + " %"
-  document.getElementById("currentBody").appendChild(currentHumidity);
-
-*/
-
-
+  //store weather data in local server with city's name
+  storeWeatherData();
+  createSearchHistory();
 }
 
 
+function storeWeatherData(){
 
+  //convert object to array and save to local storage
+  const weatherData = JSON.stringify(weatherForecast);
+  localStorage.setItem(city,weatherData)
 
+}
 
-/*
-    function getData(){
+function createSearchHistory(){
 
-      for (var j = 0; j < 40; j = j+7){
-        
-      
-      weatherForecast[i].city = data.city.name;
-      weatherForecast[i].date = data.list[j].dt_txt;
-      weatherForecast[i].icon = data.list[j].weather[0].icon;
-      weatherForecast[i].temperature = data.list[j].main.temp;
-      weatherForecast[i].wind = data.list[j].wind.speed;
-      weatherForecast[i].humidity = data.list[j].main.humidity
+  //create button for history
 
-      console.log(weatherForecast[i].city);
-      console.log(weatherForecast[i].date);
-      console.log(weatherForecast[i].icon);
-      console.log(weatherForecast[i].temperature);
-      console.log(weatherForecast[i].wind);
-      console.log(weatherForecast[i].humidity);
-      return
+}
 
-      }
-    }
-*/
+//event listener for search button
+document.getElementById("search-btn").addEventListener("click",getCityInput);
